fdca66fdcfea1fc2a4796d3239fe7c96d6598903
Jonathan Rigottier <jrigottier@gmail.com>
2022-02-28 09:20:30 +0200

    After quiz advanced homework

--- src/advancedquizz/Game.java
+++ src/advancedquizz/Game.java
@@ -1,5 +1,6 @@
 package advancedquizz;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,30 +12,101 @@ public class Game {
     // For storing all the question in one ArrayList
     private static ArrayList<Question> questions = new ArrayList<Question>();
     private static Scanner scanner = new Scanner(System.in);
+    private static int choice;
+    private static int numberOfAnswers;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        start();
         init();
         loop();
+        addQuestion();
 
     }
 
+    //Start of the game
+    private static void start(){
+        System.out.println("Please enter which quiz would like to play : ");
+        System.out.println("Type '1' for a quiz about capital's of a country -->");
+        System.out.println("Type '2' for a quiz about a country's population size -->");
+        System.out.println("Type '3' for a quiz about a city's population size -->");
+    }
+
     //Initialize game
-    private static void init(){
+    private static void init() {
 
+        choice = scanner.nextInt();
+        //Fix
+        scanner.nextLine();
+
+        BufferedReader br = null;
         try {
-            List<String> lines = Files.readAllLines(Paths.get("Quiz\\data1.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("Quiz\\data" + choice + ".txt"));
+            br = new BufferedReader(new FileReader("Quiz\\data" + choice + ".txt"));
+            numberOfAnswers = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < lines.size(); i += 4){
-                Question q;
-                q = new Question(lines.get(i), lines.get(i+1), lines.get(i+2), lines.get(i+3));
-                questions.add(q);
+            if (numberOfAnswers == 3) {
+                for (int i = 1; i < lines.size(); i += 4) {
+                    Question q;
+                    q = new Question(lines.get(i), lines.get(i + 1), lines.get(i + 2), lines.get(i + 3));
+                    questions.add(q);
+                }
+            } else if (numberOfAnswers == 4) {
+                for (int i = 1; i < lines.size(); i += 5) {
+                    Question q;
+                    q = new Question(lines.get(i), lines.get(i + 1), lines.get(i + 2), lines.get(i + 3), lines.get(i + 4));
+                    questions.add(q);
+                }
             }
-
-        }catch (Exception e){
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             System.out.println(e);
             System.out.println("Could not load file");
             System.out.println(-1);
+        }
+    }
+
+    //Add a question in the quiz
+    private static void addQuestion() throws IOException {
+
+        char answer;
+        String data;
+        do {
+            System.out.println("Would you like to add a question to the quiz : y/n ");
+            answer = scanner.next().charAt(0);
+            scanner.nextLine();
+
+        } while (answer != 'y' && answer != 'n');
+
+
+        if(answer == 'y'){
+
+            System.out.println("Add your new question :");
+            String newQuestion = scanner.nextLine();
+
+            FileOutputStream out = new FileOutputStream("Quiz\\data" + choice + ".txt", true);
+            out.write('\n');
+            for(char ch: newQuestion.toCharArray()){
+                out.write(ch);
+            }
+
+            for (int i = 0; i < numberOfAnswers; i++) {
+                System.out.println("Add your new answer here : ");
+                String possibleAnswer = scanner.nextLine();
+
+                //FileOutputStream out = new FileOutputStream("Quiz\\data" + choice + ".txt", true);
+
+                out.write('\n');
+                for(char ch: possibleAnswer.toCharArray()){
+                    out.write(ch);
+                }
+            }
+
+        }else{
+
         }
 
     }
--- src/filestream/FileOutputStreamExample.java
+++ src/filestream/FileOutputStreamExample.java
@@ -2,14 +2,17 @@ package filestream;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileOutputStreamExample {
     public static void main(String[] args) throws IOException {
 
-        String test = "Output";
+        Scanner scanner = new Scanner(System.in);
+        String test = scanner.nextLine() ;
 
-        FileOutputStream out = new FileOutputStream("Data\\Output.txt");
+        FileOutputStream out = new FileOutputStream("Data\\Output.txt", true);
 
+        out.write('\n');
         for(char ch: test.toCharArray()){
             out.write(ch);
         }
